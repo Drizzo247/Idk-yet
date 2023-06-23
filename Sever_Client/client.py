f80d7722ec728175b811eb1e4365f20b8c549ac4
Drizzo247 <alijahwalls1021@icloud.com>
2023-06-22 21:18:32 -0400

    idktbh

--- Sever_Client/client.py
+++ Sever_Client/client.py
@@ -4,7 +4,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = ("10.0.0.168")
+SERVER = ("10.0.0.167",5000)
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
